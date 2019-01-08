bfc21bdbb1d5d688db7042d01beef5f5ef8a0343
ysicing zheng <i@ysicing.me>
2019-01-08 10:21:42 +0800

    fix typo

--- prepare.sh
+++ prepare.sh
@@ -4,3 +4,117 @@ apt-get update
 apt-get install --no-install-recommends --no-install-suggests -y ufw ruby lsb-release wget curl python python-dev python-pil python-pip python-apt zip unzip openssl libssl-dev gcc libxml2 libxml2-dev libxslt1-dev zlib1g zlib1g-dev libjpeg-dev libpng-dev libffi-dev lsof libpcre3 libpcre3-dev cron
 rm -rf /var/lib/apt/lists/* 
 
+Install_Pillow()
+{
+	isSetup=`python -m PIL 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		wget -O Pillow-3.2.0.zip https://dl.spanda.io/download/Pillow-3.2.0.zip -T 10
+		unzip Pillow-3.2.0.zip
+		rm -f Pillow-3.2.0.zip
+		cd Pillow-3.2.0
+		python setup.py install
+		cd ..
+		rm -rf Pillow-3.2.0
+	fi
+	isSetup=`python -m PIL 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		echo '=================================================';
+		echo -e "\033[31mPillow installation failed. \033[0m";
+		exit;
+	fi
+}
+
+Install_psutil()
+{
+	isSetup=`python -m psutil 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		wget -O psutil-5.2.2.tar.gz https://dl.spanda.io/download/psutil-5.2.2.tar.gz -T 10
+		tar xvf psutil-5.2.2.tar.gz
+		rm -f psutil-5.2.2.tar.gz
+		cd psutil-5.2.2
+		python setup.py install
+		cd ..
+		rm -rf psutil-5.2.2
+	fi
+	isSetup=`python -m psutil 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		echo '=================================================';
+		echo -e "\033[31mpsutil installation failed. \033[0m";
+		exit;
+	fi
+}
+
+Install_mysqldb()
+{
+	isSetup=`python -m MySQLdb 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		wget -O MySQL-python-1.2.5.zip https://dl.spanda.io/download/MySQL-python-1.2.5.zip -T 10
+		unzip MySQL-python-1.2.5.zip
+		rm -f MySQL-python-1.2.5.zip
+		cd MySQL-python-1.2.5
+		python setup.py install
+		cd ..
+		rm -rf MySQL-python-1.2.5
+	fi
+	
+}
+
+Install_chardet()
+{
+	isSetup=`python -m chardet 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		wget -O chardet-2.3.0.tar.gz https://dl.spanda.io/download/chardet-2.3.0.tar.gz -T 10
+		tar xvf chardet-2.3.0.tar.gz
+		rm -f chardet-2.3.0.tar.gz
+		cd chardet-2.3.0
+		python setup.py install
+		cd ..
+		rm -rf chardet-2.3.0
+	fi
+	isSetup=`python -m chardet 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		echo '=================================================';
+		echo -e "\033[31mchardet installation failed. \033[0m";
+		exit;
+	fi
+}
+
+Install_webpy()
+{
+	isSetup=`python -m web 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		wget -O web.py-0.38.tar.gz https://dl.spanda.io/download/web.py-0.38.tar.gz -T 10
+		tar xvf web.py-0.38.tar.gz
+		rm -f web.py-0.38.tar.gz
+		cd web.py-0.38
+		python setup.py install
+		cd ..
+		rm -rf web.py-0.38
+	fi
+	
+	isSetup=`python -m web 2>&1|grep package`
+	if [ "$isSetup" = "" ];then
+		echo '=================================================';
+		echo -e "\033[31mweb.py installation failed. \033[0m";
+		exit;
+	fi
+}
+pipArg=''
+pip install --upgrade setuptools
+pip install itsdangerous==0.24
+pip install paramiko==2.0.2
+for p_name in psutil chardet virtualenv Flask Flask-Session Flask-SocketIO flask-sqlalchemy Pillow gunicorn gevent-websocket;
+do
+	pip install ${p_name}
+done
+
+pip install psutil chardet virtualenv Flask Flask-Session Flask-SocketIO flask-sqlalchemy Pillow gunicorn gevent-websocket paramiko
+Install_Pillow
+Install_psutil
+
+pip install gunicorn
+if [  -f /www/server/mysql/bin/mysql ]; then
+	pip install mysql-python
+	Install_mysqldb
+fi
+Install_chardet
