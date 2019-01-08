27c83a813192bd3b0813f87a20e3a29f620cfc14
ysicing zheng <i@ysicing.me>
2019-01-08 09:56:45 +0800

    fix typo

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get update
-apt-get install --no-install-recommends --no-install-suggests -y ufw ruby lsb-release wget curl python python-dev python-imaging python-pip python-apt zip unzip openssl libssl-dev gcc libxml2 libxml2-dev libxslt zlib1g zlib1g-dev libjpeg-dev libpng-dev libffi-dev lsof libpcre3 libpcre3-dev cron
+apt-get install --no-install-recommends --no-install-suggests -y ufw ruby lsb-release wget curl python python-dev python-pil python-pip python-apt zip unzip openssl libssl-dev gcc libxml2 libxml2-dev libxslt zlib1g zlib1g-dev libjpeg-dev libpng-dev libffi-dev lsof libpcre3 libpcre3-dev cron
 rm -rf /var/lib/apt/lists/* 
 
